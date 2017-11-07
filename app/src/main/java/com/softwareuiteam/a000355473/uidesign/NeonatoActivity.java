13e1e6a01d9b3b1a0b4692ee13f964e0c3506fc1
wanming.hu <wanming.hu1987@gmail.com>
2017-11-07 11:18:20 -0500

    New UI Design

---
+++ app/src/main/java/com/softwareuiteam/a000355473/uidesign/NeonatoActivity.java
@@ -0,0 +1,13 @@
+package com.softwareuiteam.a000355473.uidesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NeonatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_neonato);
+    }
+}
