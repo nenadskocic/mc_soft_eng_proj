b7f3b8a470c2028998cb257bf384ed658ebf7a6d
wanming.hu <wanming.hu1987@gmail.com>
2017-11-07 13:33:04 -0500

    Activity switch function added

--- app/src/main/java/com/softwareuiteam/a000355473/uidesign/NeonatoActivity.java
+++ app/src/main/java/com/softwareuiteam/a000355473/uidesign/NeonatoActivity.java
@@ -1,7 +1,9 @@
 package com.softwareuiteam.a000355473.uidesign;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class NeonatoActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class NeonatoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neonato);
+    }
+
+    public void Register(View v){
+        startActivity(new Intent(NeonatoActivity.this, RegisterActivity.class));
+        this.finish();
     }
 }
