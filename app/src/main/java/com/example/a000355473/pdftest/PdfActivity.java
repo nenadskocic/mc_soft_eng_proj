26b0683a9280cb2d6cd7d7c73a7b080f612d0c8f
wanming.hu <wanming.hu1987@Gmail.com>
2017-11-13 14:10:47 -0500

    commit

--- app/src/main/java/com/example/a000355473/pdftest/PdfActivity.java
+++ app/src/main/java/com/example/a000355473/pdftest/PdfActivity.java
@@ -1,7 +1,12 @@
 package com.example.a000355473.pdftest;
 
+import android.print.PrintManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class PdfActivity extends AppCompatActivity {
 
@@ -9,5 +14,11 @@ public class PdfActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pdf);
+    }
+
+    public void printPDF(View view) {
+        PrintManager printManager = (PrintManager) getSystemService(PRINT_SERVICE);
+        printManager.print("print_any_view_job_name", new ViewPrintAdapter(this,
+                findViewById(R.id.textView)), null);
     }
 }
