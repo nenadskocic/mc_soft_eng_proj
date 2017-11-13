1ae8765b7852dc6d9fea6ad7f6fc6fed069cc95e
wanming.hu <wanming.hu1987@Gmail.com>
2017-11-13 13:52:21 -0500

    PDF TEst

---
+++ app/src/main/java/com/example/a000355473/pdftest/PdfActivity.java
@@ -0,0 +1,13 @@
+package com.example.a000355473.pdftest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PdfActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pdf);
+    }
+}
