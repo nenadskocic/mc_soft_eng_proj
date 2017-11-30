5cf57919d9e3e8f0b1be5f89b266014415163097
maurice.de-vries <maurice.de-vries@mohawkcollege.ca>
2017-11-30 09:19:23 -0500

    WIP

---
+++ app/src/main/java/dao/PersonDAO.java
@@ -0,0 +1,8 @@
+package dao;
+
+/**
+ * Created by Maurice on 11/30/2017.
+ */
+
+public class PersonDAO {
+}
---
+++ app/src/main/java/database/AppDatabase.java
@@ -0,0 +1,11 @@
+package database;
+
+/**
+ * Created by Maurice on 11/30/2017.
+ */
+
+/*@Database(entities = {
+        Person.class
+}, version = 1)*/
+public abstract class AppDatabase {
+}
