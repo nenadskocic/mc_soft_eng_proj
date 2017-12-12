321b4eb3c57adf626b8b5e57917743b42b493277
maurice.de-vries <maurice.de-vries@mohawkcollege.ca>
2017-12-11 22:49:46 -0500

    Added data to list of patients

--- app/src/main/java/com/neonatal/app/src/MainMenuActivity.java
+++ app/src/main/java/com/neonatal/app/src/MainMenuActivity.java
@@ -4,13 +4,26 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainMenuActivity extends AppCompatActivity {
+    ArrayList<String> arrayPatients = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+
+        arrayPatients.add("Susan Example");
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayPatients);
+        ListView listViewPatients = (ListView) findViewById(R.id.listViewPatients);
+        listViewPatients.setAdapter(adapter);
     }
 
     public void NicuVideo(View v){
